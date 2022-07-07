c5838a0fb57a592392ab34a2c56ee2561d7a2638
ptiw-rep <72190379+ptiw-rep@users.noreply.github.com>
2022-07-07 09:20:46 +0530

    Time: 8 ms (71.20%), Space: 15.3 MB (14.32%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,18 +1,26 @@
 class Solution {
 public:
-    int solve(vector<int>& prices, int i, bool buy,vector<vector<int>> &dp) {
-        if( i==prices.size() )
-            return 0;
-        if( dp[i][buy]!=-1)
-            return dp[i][buy];
-        if( buy )
-            return dp[i][buy]=max(-prices[i]+ solve(prices,i+1,0,dp) , solve(prices,i+1,1,dp));
-        else
-            return dp[i][buy]=max(prices[i]+ solve(prices,i+1,1,dp), solve(prices,i+1,0,dp));
-    }
+    // int solve(vector<int>& prices, int i, bool buy,vector<vector<int>> &dp) {
+    //     if( i==prices.size() )
+    //         return 0;
+    //     if( dp[i][buy]!=-1)
+    //         return dp[i][buy];
+    //     if( buy )
+    //         return dp[i][buy]=max(-prices[i]+ solve(prices,i+1,0,dp) , solve(prices,i+1,1,dp));
+    //     else
+    //         return dp[i][buy]=max(prices[i]+ solve(prices,i+1,1,dp), solve(prices,i+1,0,dp));
+    // }
     int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        vector<vector<int>> dp(n+1,vector<int> (2,-1));
-        return solve(prices,0,1,dp);
+        int i,buy,n = prices.size();
+        vector<vector<int>> dp(n+1,vector<int> (2,0));
+        for(i=n-1;i>=0;i-- ) {
+            for( buy=0;buy<2;buy++ ) {
+                if( buy )
+                    dp[i][buy] = max( -prices[i]+ dp[i+1][0],dp[i+1][1]);
+                else
+                    dp[i][buy] = max( prices[i]+ dp[i+1][1],dp[i+1][0]);
+            }
+        }
+        return dp[0][1];
     }
 };
