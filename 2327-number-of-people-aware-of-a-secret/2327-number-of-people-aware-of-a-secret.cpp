e6b1d48a950c5b3433e8bb34d7d8a2509f30a797
ptiw-rep <72190379+ptiw-rep@users.noreply.github.com>
2022-07-08 17:43:48 +0530

    Time: 4 ms (84.58%), Space: 6.5 MB (82.23%) - LeetHub

--- 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.cpp
+++ 2327-number-of-people-aware-of-a-secret/2327-number-of-people-aware-of-a-secret.cpp
@@ -4,6 +4,9 @@ public:
     int peopleAwareOfSecret(int n, int delay, int forget) {
         vector<int> dp(n+1,0); 
         dp[1]= 1;
+        // sh = people sharing secret
+        // newShare = wo log jinko naya secret pata laga h
+        // forgot = wo log jo secret bhul gye h
         long long int sh = 0, newShare,forgot,ans=0;
         for( int i =2;i<=n;i++ ) {
             newShare = dp[max(i-delay,0)];
@@ -11,6 +14,7 @@ public:
             sh = (sh + newShare - forgot+m)%m;
             dp[i] = sh;
         }
+        // ans hoga, jitne logo ko secret yaad h, to wo h ki n- forget tk jate jao n se aur sum kr lo
         for( int i = n-forget+1;i<=n;i++) 
             ans=(ans+dp[i])%m;
         return ans; 
