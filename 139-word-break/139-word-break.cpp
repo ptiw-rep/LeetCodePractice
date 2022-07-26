c294dee431c4cb1f753495995d460580c3d34975
ptiw-rep <72190379+ptiw-rep@users.noreply.github.com>
2022-07-26 23:06:21 +0530

    Time: 7 ms (82.10%), Space: 8.2 MB (77.88%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,22 +1,33 @@
 class Solution {
 public:
-    bool solve(int i, string &s, unordered_set<string> &hash, vector<int> &dp ){
-        if( i==s.size() ){
-            return true;
-        }
-        if( dp[i]!=-1 )
-            return dp[i];
-        string curr="";
-        for(int k=i;k<s.size();k++){
-            curr+=s[k];
-            if( solve(k+1,s,hash,dp) && hash.find(curr)!=hash.end() )
-                return dp[i]=true;
-        }
-        return dp[i]=false;
-    }
+    // bool solve(int i, string &s, unordered_set<string> &hash, vector<int> &dp ){
+    //     if( i==s.size() ){
+    //         return true;
+    //     }
+    //     if( dp[i]!=-1 )
+    //         return dp[i];
+    //     string curr="";
+    //     for(int k=i;k<s.size();k++){
+    //         curr+=s[k];
+    //         if( solve(k+1,s,hash,dp) && hash.find(curr)!=hash.end() )
+    //             return dp[i]=true;
+    //     }
+    //     return dp[i]=false;
+    // }
     bool wordBreak(string s, vector<string>& wordDict) {
         unordered_set<string> hash(wordDict.begin(),wordDict.end());
-        vector<int> dp(s.size(),-1);
-        return solve(0,s,hash,dp);
+        int i,k,n = s.size();
+        vector<bool> dp(n+1, false);
+        // base case
+        dp[n] = true;
+        for(i=n-1;i>=0;i--) {
+            string curr = "";
+            for(k=i;k<n;k++) {
+                curr+=s[k];
+                if( dp[k+1] && hash.find(curr)!=hash.end() )
+                    dp[i] = true;
+            }
+        }
+        return dp[0];
     }
 };
