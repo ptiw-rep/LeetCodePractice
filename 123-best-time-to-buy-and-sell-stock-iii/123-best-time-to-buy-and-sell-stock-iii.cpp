b52966a313f4472a5226753493512d8810942758
ptiw-rep <72190379+ptiw-rep@users.noreply.github.com>
2022-07-29 21:08:21 +0530

    Time: 521 ms (39.13%), Space: 75.2 MB (85.13%) - LeetHub

---
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int i,buy,cap, n=prices.size(), k=2;
+        vector<vector<int>> after(2,vector<int> (k+1,0));
+        vector<vector<int>> curr(2,vector<int> (k+1,0));
+        for( i=n-1;i>=0;i-- ) {
+            for( buy=0;buy<2;buy++ ) {
+                for( cap=1;cap<=k;cap++ ) {
+                    if( buy )
+                        curr[buy][cap] = max( -prices[i]+after[0][cap],after[1][cap]);
+                    else
+                        curr[buy][cap] = max( prices[i]+after[1][cap-1],after[0][cap]);
+                }
+            }
+            after = curr;
+        }
+        return curr[1][k];
+    }
+};
