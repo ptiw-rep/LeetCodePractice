d4ad97d220ef5a3b82ca9f35ad25b2a78353ff28
ptiw-rep <72190379+ptiw-rep@users.noreply.github.com>
2022-07-31 15:13:29 +0530

    Time: 12 ms (37.47%), Space: 8.5 MB (94.53%) - LeetHub

---
+++ 133-clone-graph/133-clone-graph.cpp
@@ -0,0 +1,45 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        unordered_map<Node *, Node *> mp;
+        if( node==NULL )
+            return NULL;
+        queue<Node *> q;
+        Node * first = new Node(node->val);
+        mp[node] = first;
+        q.push(node);
+        while(!q.empty()) {
+            Node * curr = q.front();
+            q.pop();
+            for( auto nxt : curr->neighbors) {
+                if( mp.find(nxt)==mp.end() ) {
+                    mp[nxt] = new Node(nxt->val);
+                    q.push(nxt);
+                }
+                mp[curr]->neighbors.push_back(mp[nxt]);
+            }
+        }
+        return mp[node];
+    }
+};
