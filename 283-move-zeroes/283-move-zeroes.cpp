b755b9d3a44ed636b089997d741ed24c2bc93fbb
ptiw-rep <72190379+ptiw-rep@users.noreply.github.com>
2022-08-23 20:34:33 +0530

    Time: 37 ms (54.62%), Space: 19.4 MB (13.44%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int k = 0,n = nums.size();
+        for(int i=0;i<n;i++) {
+            if( nums[i]!=0 ) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        while( k<n ) {
+            nums[k] = 0;
+            k++;
+        }
+    }
+};
